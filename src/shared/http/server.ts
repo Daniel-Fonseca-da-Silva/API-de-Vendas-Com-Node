846eaffe5d10ce3dc2298726d8f27200f240ddc2
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-04 14:45:20 -0300

    feat: Instalado typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,6 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Erro internor de servidor',
+    });
+  },
+);
 
 app.listen(8081, () => {
   console.log('Servidor iniciado na porta 8081');
