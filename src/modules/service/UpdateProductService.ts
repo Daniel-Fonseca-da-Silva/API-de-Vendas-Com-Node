65feee7def6a9da3acc6da71133f6d059be58582
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-15 15:36:30 -0300

    feat: Servico para atualizar um produto

---
+++ src/modules/service/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../products/typeorm/repositories/ProductsRepository';
+import Product from '../products/typeorm/entities/Product';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const product = await productsRepository.findOne(id);
+    const productExists = await productsRepository.findByName(name);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    if (productExists) {
+      throw new AppError('There is already product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;
