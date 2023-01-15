57aa07d0e332398474672ace1680ae66f9af5649
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-15 01:19:52 -0300

    feat: Servico para criacao de produto

---
+++ src/modules/service/ListProductService.ts
@@ -0,0 +1,14 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../products/typeorm/repositories/ProductsRepository';
+import Product from '../products/typeorm/entities/Product';
+
+export class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const products = productsRepository.find();
+    return products;
+  }
+}
+
+export default ListProductService;
